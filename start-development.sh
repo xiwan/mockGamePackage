18cd6abcb55dec4bc61c1a658c4881670a9425e2
xiwan <5155280@qq.com>
2018-05-19 20:05:06 +0800

    fix bug

--- start-development.sh
+++ start-development.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export NODE_ENV=development
-export FOREVER_ROOT='/Users/xiwan/Documents/dev/forever'
+FOREVER_ROOT='/Users/xiwan/Documents/dev/forever'
 LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
 echo $NODE_ENV
 forever start $LOGS_PARAM ./$NODE_ENV.json
