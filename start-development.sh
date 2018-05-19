d5a603de21606aa80afbac80e75d2f3e0a5db44e
xiwan <5155280@qq.com>
2018-05-19 15:35:05 +0800

    feature: adjust forever params

--- start-development.sh
+++ start-development.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export NODE_ENV=development
-forever start ./development.json
+export FOREVER_ROOT=/Users/xiwan/Documents/dev/mockGamePackage
+export LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
+forever start $LOGS_PARAM ./$NODE_ENV.json
 forever logs
--- start-production.sh
+++ start-production.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export NODE_ENV=production
-forever start ./production.json
+export FOREVER_ROOT=/home/mockGamePackage
+export LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
+forever start $LOGS_PARAM ./$NODE_ENV.json
 forever logs
