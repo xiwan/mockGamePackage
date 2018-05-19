b02c7ee999d81eb6984ee464942d25ce0adae546
xiwan <5155280@qq.com>
2018-05-19 16:02:12 +0800

    fixbug: logs

--- start-development.sh
+++ start-development.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export NODE_ENV=development
-export FOREVER_ROOT=/Users/xiwan/Documents/dev/mockGamePackage
-export LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
+export FOREVER_ROOT=/Users/xiwan/Documents/dev/forever
+LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
+echo $NODE_ENV
 forever start $LOGS_PARAM ./$NODE_ENV.json
 forever logs
--- start-production.sh
+++ start-production.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export NODE_ENV=production
-export FOREVER_ROOT=/home/mockGamePackage
-export LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
+export FOREVER_ROOT=/home/forever
+LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
+echo $NODE_ENV
 forever start $LOGS_PARAM ./$NODE_ENV.json
 forever logs
