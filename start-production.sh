f74fd0df2973617890afc5b5488552e310b78f51
xiwan <5155280@qq.com>
2018-05-19 16:56:55 +0800

    fix bug

--- start-production.sh
+++ start-production.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export NODE_ENV=production
-export FOREVER_ROOT=/home/forever
+export FOREVER_ROOT='/home/forever'
 LOGS_PARAM='-l ./logs/forever.log -o ./logs/out.log -e ./logs/err.log'
 echo $NODE_ENV
 forever start $LOGS_PARAM ./$NODE_ENV.json
